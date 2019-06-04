97b4ed81e23c48fb74a509dfd43e1ce35aa8f9e5
Martha28 <45656448+Martha28@users.noreply.github.com>
2019-06-04 13:06:21 +0530

    Update App.java

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -6,8 +6,10 @@ package com.stackroute;
  */
 public class App 
 {
+    public static hello(){
+        System.out.println( "Hello World!" );
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
